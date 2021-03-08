9d0fc8ce64a89e2318e6e655d4bbf35f231574d1
Tobias Ostergren <ostergrentobias@gmail.com>
2021-03-08 11:03:19 +0100

    Added routes
    Added routes and simple content on views

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // pages
+import AboutUs from './pages/AboutUs'
 import Home from './pages/Home'
+import Locations from './pages/Locations'
+import MyPage from './pages/MyPage'
+
+
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <header className="App-header">
-
-        </header>
+        <header className="App-header"><NavBar /></header>
 
         <main>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/Om-oss" component={AboutUs} />
+            <Route exact path="/Platser" component={Locations} />
+            <Route exact path="/Mina-sidor" component={MyPage} />
           </Switch>
         </main>
 
---
+++ src/pages/MyPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '../components/Login'
+
+const MyPage = () => {
+  return (
+    <div>
+      <h1>Mina sidor</h1>
+    </div >
+  );
+}
+
+export default MyPage;
