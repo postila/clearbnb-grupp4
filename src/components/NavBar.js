0038b2458e408446bde3d3a9f1ed56e6121590ac
Kristian <balubbas@gmail.com>
2021-03-08 13:51:50 +0100

    Update NavBar.js
    Added radium, responsive navbar. My pages on the right
    
    Co-Authored-By: Tobias Östergren <45110274+volture1@users.noreply.github.com>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,71 @@
 import { Link, useHistory } from 'react-router-dom'
+import Radium from 'radium'
 
 
-export default function NavBar() {
+function NavBar() {
 
 
 
   return (
-    <nav style={styles}>
-      <Link style={{ textDecoration: 'none', color: '#eee', marginRight: '40px' }} to="/Om-oss">Om oss</Link>
-      <Link style={{ textDecoration: 'none', color: '#eee', marginRight: '40px' }} to="/">Hem</Link>
-      <Link style={{ textDecoration: 'none', color: '#eee', marginRight: '40px' }} to="/Platser">Platser</Link>
-      <Link style={{ textDecoration: 'none', color: '#eee', marginRight: '40px' }} to="/Mina-sidor">Mina sidor</Link>
+    //<div style={{ width: '100%', margin: '0 auto', backgroundColor: 'red' }}>
+    /* <div style={styles.container}>
+    <nav style={styles.navbar}>
+      <div style={styles.left}>12343</div>
+      <div style={styles.button}>
+        <Link to="/Om-oss ">Om oss</Link>
+        <Link to="/">Hem </Link>
+        <Link to="/Platser">Platser</Link>
+      </div>
+      <div style={styles.dropdown}>
+        <Link  to="/Mina-sidor">Mina sidor</Link>
+      </div>
+      </nav> 
+      </div> */
+    <div>
+    <nav style={styles.navbar}>
+      <Link style={styles.button} to="/Om-oss">Om oss</Link>
+      <Link style={styles.button} to="/">Hem</Link>
+      <Link style={styles.button} to="/Platser">Platser</Link>
+    
+      <div style={styles.mypage}>
+          <Link style={{ color: 'white' }} to="/Mina-sidor">Mina sidor</Link>
+      </div>
+    
+      </nav>
+    </div>
 
-    </nav>
   )
 }
 
 const styles = {
-  display: 'flex',
+navbar: {
+  //display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
   height: '100%',
-  fontFamily: 'Verdana'
+  fontFamily: 'Verdana',
+  textDecoration: 'none',
+  color: '#eee',
+  marginRight: '40px',
+  paddingTop: '15px'
+  
+  },
+button: {
+  marginRight: '40px',
+  color: '#eee',
+  },
+mypage: {
+  color: '#eee',
+  float: 'right',
+  
+  },
+
+
+} 
+
+
+const navDropDown = {
+
 }
+
+export default Radium(NavBar);
