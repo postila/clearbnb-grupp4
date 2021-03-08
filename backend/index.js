58e1b856616d8597f995ab09b852f5f0e085018a
postila <isabella.postila@hotmail.com>
2021-03-08 13:55:37 +0100

    Connect to MongoDB Atlas
    Connected Express with Atlas
    
    Co-Authored-By: nicolebradleey <72066187+nicolebradleey@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,29 @@
+global.mongoose = require('mongoose')
 const express = require('express')
+const { users } = require('./models.js')
 const app = express()
 
 app.use(express.json())
 
+const atlasUrl = 'mongodb+srv://clearbnb-grupp4:grupp4@cluster0.tzp9m.mongodb.net/clearbnb?retryWrites=true&w=majority'
 
+global.mongoose.connect(atlasUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const models = require('./models.js')
+
+const janeDoe = new users({
+  name: 'Jane Doe',
+  email: 'janedoe@haha.se',
+  password: '123'
+})
+
+app.post('/api/users', async (req, res) => {
+  let doc = new users(req.body)
+  await doc.save()
+  res.json(doc)
+})
+
+app.listen(3001, () => console.log('Server stated on port 3001'))
