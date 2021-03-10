8770de5f4888ae64e341c9d62d33fd17ac853de5
postila <isabella.postila@hotmail.com>
2021-03-10 11:00:30 +0100

    Find accommodation and render to detailspage
    Co-Authored-By: nicolebradleey <72066187+nicolebradleey@users.noreply.github.com>

--- src/pages/AccommodationsDetails.js
+++ src/pages/AccommodationsDetails.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AccommodationsContext } from '../contexts/AccommodationsContext'
+import { useParams } from 'react-router-dom';
+import Radium from 'radium'
 
-const AccommodationsDetails = () => {
+
+const AccommodationsDetails = (props) => {
+  const { accommodations } = useContext(AccommodationsContext)
+  const { id } = useParams()
+  const accommodation = accommodations.find(accommodation => accommodation._id === id)
+  console.log({ id }, 'id från detaljsida');
+  // const accommodationItem = () => {
+  //   accommodations.filter(accommodation => accommodation._id === id)
+  // }
+
   return (
     <div>
       <h1>Details Page</h1>
+      <h1>{ accommodation.title }</h1>
     </div>
   );
 }
