8cd4271b960eaf048aa44412fe241b2b0d943e5a
postila <isabella.postila@hotmail.com>
2021-03-10 10:15:32 +0100

    Added onClick på details page
    Co-Authored-By: nicolebradleey <72066187+nicolebradleey@users.noreply.github.com>

--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -1,15 +1,28 @@
 import React, { useContext } from 'react';
 import { AccommodationsContext } from '../contexts/AccommodationsContext'
+import Radium from 'radium'
+import { useHistory } from 'react-router-dom'
 
 const Locations = () => {
   const { accommodations } = useContext(AccommodationsContext)
+  const history = useHistory()
+
+  const goToDetailsPage = (id) => {
+    history.push('/accommodationsDetails/' + id)
+  }
 
   const card = accommodation => (
     <div
+      style={styles.box}
       className="card"
       key={accommodation._id}
+      onClick={() =>  goToDetailsPage (accommodation._id)}
     >
-      <h1>{ accommodation.title }</h1>
+      <img style={styles.pictures} src={accommodation.imageUrl} />
+      <div style={styles.text}>
+        <h3>{ accommodation.title }</h3>
+        <p>Gäster {accommodation.maxGuest} | {accommodation.pricePerNight} SEK</p>
+      </div>
     </div>
   )
 
@@ -20,4 +33,30 @@ const Locations = () => {
   );
 }
 
-export default Locations;
+const styles = {
+  box: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridGap: '20px',
+    maxWidth: '800px',
+    background: '#202329',
+    padding: '15px',
+    margin: '10px auto',
+    borderRadius: '15px',
+    ':hover': {
+      opacity: '80%',
+      cursor: 'pointer'
+    }
+  },
+  pictures: {
+    maxWidth: '45vh',
+    borderRadius: '10px',
+    boxShadow: '1px 1px 10px 0.5px #343841'
+  },
+  text: {
+    textAlign: 'left',
+    color: 'white'
+  }
+}
+
+export default Radium(Locations);
