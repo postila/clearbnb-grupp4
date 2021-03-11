78d8cd6c1ead227fe7c2d9d2aa12dbc82fcd52fb
Hugo Granath <72066319+mrHugolo@users.noreply.github.com>
2021-03-11 21:40:54 +0100

    saving amenities in array

--- src/components/Amenities.js
+++ src/components/Amenities.js
@@ -3,19 +3,28 @@ import { AmenitiesContext } from '../contexts/AmenitiesContext'
 import { useHistory } from 'react-router-dom'
 import Radium from 'radium'
 
-function Amenities() {
+function Amenities(props) {
+  let amenityList = []
   const { amenities } = useContext(AmenitiesContext)
   const history = useHistory()
+
   const amenityItem = amenity => (
     <div className="amenityItem" key={amenity._id} style={amenityItemStyle}>
       <input
-      type="checkbox"
-      value={amenity._id}
-      style={checkBoxStyle}></input>
-      <div style={{float: 'left', paddingBottom: '10px'}}>
-        <img style={{ height: '30px'}} src={amenity.iconUrl} alt={'picture ' + amenity._id} />
+        type="checkbox"
+        value={amenity._id}
+        onClick={() => {
+          let index = amenityList.indexOf(amenity._id)
+          console.log(index, 'index')
+          if (index == -1) { amenityList.push(amenity._id) }
+          else { amenityList.splice(index, 1) }
+          console.log(amenityList, amenity.name,)
+        }}
+        style={checkBoxStyle}></input>
+      <div style={{ float: 'left', paddingBottom: '10px' }}>
+        <img style={{ height: '30px' }} src={amenity.iconUrl} alt={'picture ' + amenity._id} />
       </div>
-      
+
       <div style={{ float: 'left', paddingLeft: '10px' }}>
         <p>{amenity.name}</p>
       </div>
@@ -35,12 +44,15 @@ const amenityItemStyle = {
 const amenitiesListStyle = {
   display: 'grid',
   color: 'white',
-  justifyContent: 'center'
+  justifyContent: 'center',
+  gridTemplateColumns: 'repeat(4, 1fr)',
+  gridGap: '25px'
 }
 const checkBoxStyle = {
   float: 'left',
   marginRight: '10px',
+  marginTop: '7px',
+  backGroundColor: 'black'
 }
-
 
 export default Radium(Amenities)
