745ea61fd17ba92c66d034f065852e81af3354d5
nicolebradleey <nicole_bradley94@hotmail.com>
2021-03-11 15:20:03 +0100

    Styled about us page.
    Co-Authored-By: postila <72066238+postila@users.noreply.github.com>

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import Login from '../components/Login'
+import Radium from 'radium'
+import background from '../background.jpg'
 
 
 const AboutUs = () => {
 
   // const handleClick = useState()
   return (
-    <div>
-      <h1>om oss</h1>
+    <div style={{ backgroundImage: `url(${background})`, height: '100vh', margin: '0' }}>
+      <div style={styles.box}>
+        <p style={styles.content}>Cras facilisis urna ornare ex volutpat, et
+        convallis erat elementum. Ut aliquam, ipsum vitae
+        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+        metus nec massa. Maecenas hendrerit laoreet augue
+        nec molestie. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus.</p>
+      </div>
     </div >
   );
 }
 
-export default AboutUs;
+const styles = {
+  box: {
+    display: 'inline-block',
+    float:'left',
+    margin: '30vh 0 0 20vh',
+    maxWidth: '400px',
+    backgroundColor: 'white',
+    opacity: '30%',
+    padding: '30px',
+    borderRadius:'10px'
+
+  },
+  content: {
+    color: 'black',
+    margin: '0',
+    fontWeight: 'bold',
+    letterSpacing:'0.5px'
+  },
+  background: {
+    backgroundImage:''
+  }
+  
+}
+
+export default Radium(AboutUs);
