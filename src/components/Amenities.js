eb495cdb4f590b9ee1ef9742e14d519b574bd75d
Kristian <balubbas@gmail.com>
2021-03-11 08:32:46 +0100

    formatting

--- src/components/Amenities.js
+++ src/components/Amenities.js
@@ -7,26 +7,12 @@ function Amenities() {
   const { amenities } = useContext(AmenitiesContext)
   const history = useHistory()
   const amenityItem = amenity => (
-    <div
-      className="amenityItem"
-      key={amenity._id}
-      style={amenityItemStyle}
-    >
-      <div style={{
-        float: 'left',
-        paddingBottom: '10px'
-      }}>
-        <img style={{
-          height: '30px'
-        }}
-          src={amenity.iconUrl}
-          alt={'picture ' + amenity._id}
-        />
+    <div className="amenityItem" key={amenity._id} style={amenityItemStyle}>
+      <div style={{float: 'left', paddingBottom: '10px'}}>
+        <img style={{ height: '30px'}} src={amenity.iconUrl} alt={'picture ' + amenity._id} />
       </div>
-      <div style={{
-        float: 'left',
-        paddingLeft: '10px'
-      }}>
+
+      <div style={{ float: 'left', paddingLeft: '10px' }}>
         <p>{amenity.name}</p>
       </div>
 
@@ -46,9 +32,6 @@ const amenitiesListStyle = {
   display: 'grid',
   color: 'white',
   justifyContent: 'center'
-  //justifyContent: 'space-between',
-  //flexWrap: 'wrap',
-  //width: '100%'
 }
 
 
