b5642518e1f23b4ca9d9c2014d85a043125cb8bb
Tobias Ostergren <ostergrentobias@gmail.com>
2021-03-12 13:53:42 +0100

    small update to accommodationcontext

--- src/contexts/AccommodationsContext.js
+++ src/contexts/AccommodationsContext.js
@@ -4,7 +4,7 @@ export const AccommodationsContext = createContext();
 
 export default function AccommodationsContextProvider(props) {
   const [accommodations, setAccommodations] = useState([])
-  
+  const [accommodationId, setAccommodationId] = useState([])
   const fetchAccommodations = async () => {
     let res = await fetch('/rest/accommodations')
     res = await res.json()
@@ -14,26 +14,29 @@ export default function AccommodationsContextProvider(props) {
   const addAccommodation = async accommodation => {
     let res = await fetch('/api/accommodations', {
       method: 'POST',
-      headers: {'content-type': 'application/json'},
+      headers: { 'content-type': 'application/json' },
       body: JSON.stringify(accommodation)
     })
-
     res = await res.json()
+    //console.log(res._id)
+    setAccommodationId(res._id)
+    console.log(accommodationId + ' sdfdsf')
+
     setAccommodations([...accommodations, accommodation])
   }
 
   useEffect(() => {
     fetchAccommodations()
   }, [])
-  
+
   const values = {
     accommodations,
     addAccommodation
   }
-  
+
   return (
     <AccommodationsContext.Provider value={values}>
       {props.children}
     </AccommodationsContext.Provider>
-)
+  )
 }
