906fda44217af64cd6974d5c4a24e5032d52f73e
nicolebradleey <nicole_bradley94@hotmail.com>
2021-03-16 11:22:56 +0100

    Update to match develop

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,9 @@
-import { useContext, useRef } from 'react';
+import { useContext, useRef, useState } from 'react';
 import { UserContext } from '../contexts/UserContextProvider'
 
-const Register = () => {
+const Register = (props) => {
   const { addUser } = useContext(UserContext)
+  const [error, setError] = useState(false)
 
   const name = useRef()
   const email = useRef()
@@ -18,17 +19,22 @@ const Register = () => {
       password: password.current.value
     }
 
-    if (password.current.value === confirmPassword.current.value) {
-      await addUser(user)
+    if (password.current.value !== confirmPassword.current.value) {
+      setError(true)
+      name.current.value = ''
+      email.current.value = ''
+      password.current.value = ''
+      confirmPassword.current.value = ''
       return;
     }
+    await addUser(user)
   }
 
   return (
     <form key="1" style={styles.form} onSubmit={createUser}>
-      <input key="2" ref={ name } style={ styles.input } type="text" placeholder="Namn" required></input>
-      <input key="3" ref={ email } style={ styles.input } type="email" placeholder="E-mail" required></input>
-      <input key="4" ref={ password } style={ styles.input } type="password" placeholder="Lösenord" required></input>
+      <input key="2" ref={name} style={styles.input} type="text" placeholder="Namn" required></input>
+      <input key="3" ref={email} style={styles.input} type="email" placeholder="E-mail" required></input>
+      <input key="4" ref={password} style={styles.input} type="password" placeholder="Lösenord" required></input>
       <input key="5" ref={confirmPassword} style={styles.input} type="password" placeholder="Bekräfta lösenord" required></input>
       {error && <p style={styles.error}>Lösenordet matchar inte</p>}
       <div>
@@ -48,16 +54,44 @@ const styles = {
     padding: '10px'
   },
   input: {
-    textAlign: 'center'
+    textAlign: 'center',
+    padding: '10px',
+    borderRadius: '10px',
+    border: 'none',
+    ':focus': {
+      outline: 'none'
+    }
   },
   button: {
-    maxWidth: '100px',
-    margin: '0 auto',
-    marginTop: '10px',
+    maxWidth: '150px',
+    margin: '10px',
     cursor: 'pointer',
     border: 'none',
     padding: '10px',
+    borderRadius: '10px',
+    background: '#202329',
+    color: 'white',
+    textTransform: 'uppercase',
+    fontSize: '12px',
+    fontWeight: 'bold'
+  },
+  error: {
+    background: '#202329',
+    color: 'white',
+    padding: '10px',
+    fontWeight: '700',
+    maxWidth: '250px',
+    margin: '10px auto',
     borderRadius: '10px'
+  },
+  logIn: {
+    cursor: 'pointer',
+    fontSize: '12px',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    ':hover': {
+      opacity: '50%'
+    }
   }
 }
 
