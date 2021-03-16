3ab514ef958f2e14d64af24d91c83fd94a3981cb
postila <isabella.postila@hotmail.com>
2021-03-16 13:50:45 +0100

    Calculate prices for a booking
    Co-Authored-By: Hugo Granath <72066319+mrHugolo@users.noreply.github.com>

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,8 +1,9 @@
-import React, { useState, useContext, useRef } from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import Radium from 'radium'
 import { BookingContext } from '../contexts/BookingContextProvider';
 import DatePicker from "react-datepicker";
 import { useHistory } from 'react-router-dom'
+import { logDOM } from '@testing-library/dom';
 
 
 const BookingForm = (props) => {
@@ -13,25 +14,29 @@ const BookingForm = (props) => {
   const [accommodationMaxGuests, setAccommodationMaxGuests] = useState(props.accommodation.maxGuests)
   const [validatDates, setValidatDates] = useState(true)
   const [bookingOk, setBookingOk] = useState(false)
+  const [price, setPrice] = useState()
+  const dayInMilliSec = 86400000;
 
   const [arrDate, setArrDate] = useState()
   const [depDate, setDepDate] = useState()
   const guests = useRef()
 
-  const calculateTotalPrice = (price, nights) => {
-    return (price * nights) * 1.15;
+  const handlePrice = () => {
+    if (arrDate && depDate) {
+      setPrice(((depDate.getTime() - arrDate.getTime() )/ dayInMilliSec) * accommodationPrice)
+      console.log(price, 'från handlePrice');
+      return price
+    }
   }
-
+ 
   const createBooking = async e => {
     e.preventDefault()
-
     const booking = {
       // user: 
       accommodation: accommodationId,
       startDate: arrDate.getTime(),
       endDate: depDate.getTime(),
       guests: guests.current.value,
-      // totalPrice: accommodationPrice
     }
 
     if (arrDate.getTime() < depDate.getTime()) {
@@ -43,8 +48,11 @@ const BookingForm = (props) => {
       setValidatDates(false)
       setBookingOk(false)
     }
-    console.log(booking, 'bokningen')
   }
+
+  useEffect(() => {
+    handlePrice()
+  }, [arrDate, depDate])
 
   return (
     <div>
@@ -56,8 +64,18 @@ const BookingForm = (props) => {
           max={accommodationMaxGuests}
           min="1"
           style={styles.input} /><br />
-        <DatePicker selected={arrDate} onChange={data => setArrDate(data)} placeholderText="Ankomst" style={styles.input} /> <br />
-        <DatePicker selected={depDate} onChange={data => setDepDate(data)} placeholderText="Avresa" style={styles.input} /> <br />
+        <DatePicker selected={arrDate} onChange={data => setArrDate(data)} placeholderText="Ankomst" dateFormat="yyyy/MM/dd" /> <br />
+        <DatePicker selected={depDate} onChange={data => setDepDate(data)} placeholderText="Avresa" dateFormat="yyyy/MM/dd" /> <br />
+
+        {arrDate && depDate &&
+          <div>
+            <p>Pris: {Math.round(price)} SEK</p>
+          <p>Serviceavgift: {Math.round((price * 0.15))} SEK</p>
+          <p>Totalpris: {Math.round((price * 1.15))}</p>
+          
+          </div>
+        }
+
         <button style={styles.button}>Boka</button>
         {!validatDates && <p style={styles.error}>Datum för avresa kan inte ske före ankomstdatum.</p>}
         {bookingOk && <p style={styles.ok}>Bokningen genomförds!</p>}
