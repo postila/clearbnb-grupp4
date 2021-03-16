f07656d741020d30c1c71ef4b8aa1674ab24fcf1
nicolebradleey <nicole_bradley94@hotmail.com>
2021-03-16 10:57:51 +0100

    Background added to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import Login from '../components/Login'
 import Register from '../components/Register';
 import { LocationContext } from '../contexts/locationContextProvider'
 import LocationList from '../components/LocationList'
+import Background from '../homeBackground.jpg'
+
 const Home = () => {
   const [register, setRegister] = useState(false)
 
@@ -11,23 +13,31 @@ const Home = () => {
   }
 
   return (
+    <div style={{ backgroundImage: `url(${Background})`, height: '100vh', margin: '0', marginTop:'-20px' }}>
     <div style={{ width: '100%' }}>
       <div className="login-register-form">
         {!register && <Login displayRegisterForm={displayRegisterForm} />}
         {register && <Register />}
       </div>
-      <div style={locationListStyle}>
+      <div style={styles.locationList}>
         <LocationList />
       </div>
-    </div >
+      </div >
+    </div>
   );
 }
-const locationListStyle = {
-  display: 'grid',
-  gridGap: '10px 10px',
-  justifyContent: 'center',
-  //flexWrap: 'wrap',
-  width: '100%'
+const styles = {
+  locationList: {
+    display: 'grid',
+    gridGap: '10px 10px',
+    justifyContent: 'center',
+    //flexWrap: 'wrap',
+    width: '100%'
+  },
+  background: {
+    backgroundImage: '',
+
+  }
 }
 
 export default Home;
