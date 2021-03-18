0770b1b644f9e99a9baaa8641659a8d5d2e8c923
Tobias Ostergren <ostergrentobias@gmail.com>
2021-03-18 16:46:49 +0100

    added logout in dropdown menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,7 +34,7 @@ const Home = () => {
         {!register && <Login displayRegisterForm={toggleRegisterForm} />}
         {register && <Register displayRegisterForm={toggleRegisterForm} />}
       </div>}
-      <button onClick={logOut}>Logga ut</button>
+      
       <div style={styles.locationList}>
         {userId && <LocationList />}
       </div>
