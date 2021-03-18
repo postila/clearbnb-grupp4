3c8b6d4c698b1ee3594f3c633bf0d964e5d6064b
postila <isabella.postila@hotmail.com>
2021-03-18 10:25:08 +0100

    Update index.js
    Created app.get CRUD to fetch all dates

--- backend/index.js
+++ backend/index.js
@@ -87,47 +87,15 @@ app.get('/rest/bookings', async (req, res) => {
   res.json(docs)
 })
 
+app.get('/rest/dates', async (req, res) => {
+  let docs = await rentaldates.find()
+  res.json(docs)
+})
+
 app.post('/rest/dates', async (req, res) => {
   let doc = new rentaldates(req.body)
   await doc.save()
   res.json(doc)
-})
-
-app.get('/api/login', async (req, res) => {
-  if (session("current-member") != null) {
-    res.send("Already logged in");
-    return;
-  }
-
-  let doc = new users(req.body)
-  session("current-member", doc)
-  // let user = await users(req.body);
-
-  // User = collection("User").findOne(ObjectFilters.eq("email", user.getEmail()));
-  // if (userInColl == null) {
-  // No user with matching email
-  // res.json(User);
-  // await doc.save()
-  res.json(doc);
-  //   return;
-  // }
-
-  // validate password
-  // if (HashPassword.match(user.getPassword() + secretSalt, userInColl.getPassword())) {
-  //   // correct -> login
-  //   //userInColl.setPassword(null);
-  //   req.session("current-user", userInColl);
-  //   res.json(userInColl);
-  // } else {
-  //   // wrong password
-  //   res.send("Bad credentials");
-  // }
-
-})
-
-app.get('/api/logout', async (req, res) => {
-  session("current-member")
-  res.send('Logged out')
 })
 
 app.listen(3001, () => console.log('Server stated on port 3001'))
