d34c6cc33ff20ca510444c9be1fff13aac4fbde1
Kristian <the.dyze@gmail.com>
2021-03-19 09:46:44 +0100

    Update BookingsList.js

--- src/components/BookingsList.js
+++ src/components/BookingsList.js
@@ -1,11 +1,18 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { BookingContext } from '../contexts/BookingContextProvider'
 import { LocationContext } from '../contexts/locationContextProvider'
+import { UserContext } from '../contexts/UserContextProvider'
 import Radium from 'radium'
 
 function BookingsList() {
   const { bookings } = useContext(BookingContext)
   const { locations } = useContext(LocationContext)
+  const { userId } = useContext(UserContext)
+  const [id, setId] = useState()
+
+  useEffect(() => {
+    setId(userId)
+  }, [userId])
 
   useEffect(() => {
     
