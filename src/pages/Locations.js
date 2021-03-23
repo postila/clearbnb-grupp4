c4139466911aa18e69ef9bbac6fb446179d600cd
nicolebradleey <nicole_bradley94@hotmail.com>
2021-03-23 15:58:48 +0100

    Unfinished filter for maxGuests
    Co-Authored-By: Kristian S <72066289+thedyze@users.noreply.github.com>
    Co-Authored-By: Hugo Granath <72066319+mrHugolo@users.noreply.github.com>

--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -1,4 +1,4 @@
-import React, { useRef, useContext, useState } from 'react';
+import React, { useRef, useContext, useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { AccommodationsContext } from '../contexts/AccommodationsContext'
 import Radium from 'radium'
@@ -6,23 +6,26 @@ import { LocationContext } from '../contexts/locationContextProvider'
 
 const Locations = () => {
   const { accommodations } = useContext(AccommodationsContext)
+  const { locations } = useContext(LocationContext)
   const { id } = useParams()
   const [selectedLocation, setSelectedLocation] = useState(id)
-  const { locations } = useContext(LocationContext)
   const location = useRef()
   const history = useHistory()
+  const [maxGuests, setMaxGuests] = useState()
 
-  async function handleChange(data)  {
-     await setSelectedLocation(data)
-     await  history.push('/Platser/' + data) 
-     console.log(data)
+  async function handleChange(data) {
+    await setSelectedLocation(data)
+    await history.push('/Platser/' + data)
+    console.log(data)
   }
 
 
   const goToDetailsPage = (id) => {
     history.push('/accommodationDetails/' + id)
   }
-  const accommodationList = id ? accommodations.filter(a => a.location._id === selectedLocation) : accommodations
+  let [accommodationList, setAccommodationList] = useState([...accommodations])
+
+
   const card = accommodation =>
 
   (
@@ -42,34 +45,26 @@ const Locations = () => {
   )
 
 
-  // const accommodation = useRef()
- 
 
+  useEffect(() => {
+    console.log(maxGuests, 'hej')
+    console.log(id, 'id')
+    if (id) setAccommodationList([accommodationList.filter(a => a.location._id === selectedLocation)])
+    if (maxGuests) setAccommodationList([accommodationList.filter(a => a.maxGuests >= maxGuests)])
+  }, [maxGuests, id, selectedLocation])
 
 
   const locationItem = location => (
     <option
-    defaultValue='hej'
-    onChange={data => setSelectedLocation(data)}
-    value={location._id}> {location.name}</option>
-    )
-    
-    // if (select === accommodation.location) {
-      
-      // }
-      // const accommodationPrice = accommodation => (
-        
-        //   <option value={accommodation._id} >{accommodation.pricePerNight}</option>
-        // )
-        // const guests = accommodation => (
-          //   <option value={accommodation._id} >{accommodation.maxGuests}</option>
-          // )
-          
-          
-          
-          
-          return (
-            <div>
+      onChange={data => setSelectedLocation(data)}
+      value={location._id}> {location.name}</option>
+  )
+
+  
+
+
+  return (
+    <div>
       <form key="1">
         <select
           key="2"
@@ -78,25 +73,37 @@ const Locations = () => {
           value={selectedLocation}
           options={locations}
           onChange={e => handleChange(e.target.value)}
-          >
+        >
 
-      <option value="" disabled selected>Välj Ort</option>
+          <option value="" disabled selected>Välj Ort</option>
           {locations.map(location => locationItem(location))}
 
         </select>
-         {/* <h1>{selectedLocation}</h1> */}
+        {/* <h1>{selectedLocation}</h1> */}
         {/* <select
           key="3"
           required ref={accommodation}
           style={styles.input}>
           {accommodations.map(accommodation => accommodationPrice(accommodation))}
-        </select>
-        <select
+        </select>*/}
+        {/* <select
           key="4"
-          required ref={accommodation}
-          style={styles.input}>
+          required ref={accommodations}
+          style={styles.input}
+          value={selectedMaxGuests}
+          options={accommodations.maxGuests}
+          onChange={e => handleChange(e.target.value)}>
           {accommodations.map(accommodation => guests(accommodation))}
         </select> */}
+        <input
+          key="5"
+          style={styles.input}
+          value={maxGuests}
+          onChange={e => setMaxGuests(e.target.value)}
+          type="number"
+          min="1"
+          placeholder='Max antal gäster'
+        ></input>
       </form>
       {accommodationList.map(accommodation => card(accommodation))}
     </div>
