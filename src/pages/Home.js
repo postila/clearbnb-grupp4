c9706b26197b244b8d07589e1f29928501959fe2
nicolebradleey <nicole_bradley94@hotmail.com>
2021-03-24 14:30:43 +0100

    Homepage for ipad

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -56,7 +56,8 @@ const styles = {
   },
   bodyStyle: {
     width: '100%',
-    background: 'linear-gradient(#62caed 0%, #ffffff 30%)'
+    background: 'linear-gradient(#62caed 0%, #ffffff 30%)',
+    backgroundSize: 'cover'
   },
   logo: {
     paddingTop: '20px',
