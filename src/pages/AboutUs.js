f61149664e35d42b6fbc4e1be5136f8dabe6125e
nicolebradleey <nicole_bradley94@hotmail.com>
2021-03-24 14:15:01 +0100

    Design for about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import Radium from 'radium'
+import Background from '../aboutUsBackground.jpg'
+
 
 const AboutUs = () => {
 
   return (
     <div style={styles.background}>
-      <div style={styles.box}>
-        <p style={styles.content}>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
+      <div style={{
+        backgroundImage: `url(${Background})`, backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', height: '100vh', margin: '0', backgroundPosition: 'center'
+      }}>
+        <div style={{ backgroundColor: 'rgba(255,255,255,0.6)' }}>
+          <div style={styles.box}>
+          <h1 style={styles.header}>Om oss</h1>
+            <p style={styles.content}>ClearBnB skapades 2021. Vårt mål är att ha boenden i hela världen vid 2027. </p><br/>
+            <p style={styles.content}>Hitta oss</p><br/>
+            <p style={styles.content}>ClearBnB<br />Malmögatan 5<br />541 25 Malmö</p>
+          
       </div>
-    </div >
+      </div >
+      </div>
+    </div>
   );
 }
 const styles = {
   box: {
-    display: 'inline-block',
+
     float: 'left',
     margin: '30vh 0 0 20vh',
     maxWidth: '400px',
     backgroundColor: 'white',
-    opacity: '30%',
+    opacity: '45%',
     padding: '30px',
     borderRadius: '10px'
   },
@@ -31,11 +39,23 @@ const styles = {
     color: 'black',
     margin: '0',
     fontWeight: 'bold',
-    letterSpacing: '0.5px'
+    letterSpacing: '0.5px',
+    fontFamily: 'Quicksand',
+     textAlign:'left'
   },
+  header: {
+    color: 'black',
+    margin: '0',
+    paddingBottom:'30px',
+    fontWeight: 'bold',
+    letterSpacing: '0.5px',
+    fontFamily: 'Quicksand',
+    textAlign:'left'
+
+  },
+  
   background: {
-    backgroundImage: '../background.jpg',
-    height: '100vh'
+    backgroundImage:''
   }
 }
 
