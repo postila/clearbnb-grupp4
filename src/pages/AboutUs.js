dbdfc74d0cf68f6aff965b86a530423ba396db06
nicolebradleey <nicole_bradley94@hotmail.com>
2021-03-25 13:25:54 +0100

    Small styling fix
    Co-Authored-By: postila <72066238+postila@users.noreply.github.com>
    Co-Authored-By: Kristian S <72066289+thedyze@users.noreply.github.com>
    Co-Authored-By: Hugo Granath <72066319+mrHugolo@users.noreply.github.com>

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -11,26 +11,27 @@ const AboutUs = () => {
         backgroundImage: `url(${Background})`, backgroundSize: 'cover', filter: 'grayscale(.5) opacity(0.8) ',
         backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', height: '100vh', margin: '0', backgroundPosition: 'center'
       }}>
-        
-          <div style={styles.box}>
-            <h1 style={styles.header}>Om oss</h1>
-            <p style={styles.content}>ClearBnB skapades 2021. Vårt mål är att ha boenden i hela världen vid 2027. </p><br />
-            <p style={styles.content}>Hitta till oss:</p><br />
-            <p style={styles.content}>ClearBnB<br />Malmögatan 5<br />541 25 Malmö</p>
 
-          </div>
-        </div >
-      </div>
+        <div style={styles.box}>
+          <h1 style={styles.header}>Om oss</h1>
+          <p style={styles.content}>ClearBnB skapades 2021. Vårt mål är att ha boenden i hela världen vid 2027. </p><br />
+          <p style={styles.content}>Hitta till oss:</p><br />
+          <p style={styles.content}>ClearBnB<br />Malmögatan 5<br />541 25 Malmö</p>
+
+        </div>
+      </div >
+    </div>
   );
 }
 const styles = {
   box: {
-    display: 'grid',
-    float: 'left',
-    margin: '30vh 0 0 20vh',
-   maxWidth: '400px',
+    position: 'absolute',
+    left: '15vw',
+    top: '30vh',
+    right: '15vw',
+    maxWidth: '400px',
     backgroundColor: 'white',
-    opacity: '45%',
+    opacity: '55%',
     padding: '30px',
     borderRadius: '10px',
   },
